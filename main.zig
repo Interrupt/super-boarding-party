6c5c7d5fd3f6948e8b5c767e21ac38c9dc5176d8
Interrupt <cuddigan@gmail.com>
2024-10-05 14:24:59 -0700

    Cleaning up main.zig

--- main.zig
+++ main.zig
@@ -10,39 +10,8 @@ const renderer = @import("game/renderer.zig");
 const graphics = delve.platform.graphics;
 const math = delve.math;
 
-var entity_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 pub var game_instance: game.GameInstance = undefined;
 pub var render_instance: renderer.RenderInstance = undefined;
-
-var camera: delve.graphics.camera.Camera = undefined;
-var fallback_material: graphics.Material = undefined;
-
-var time: f64 = 0.0;
-
-// the quake map
-var quake_map: delve.utils.quakemap.QuakeMap = undefined;
-
-// meshes for drawing
-var map_meshes: std.ArrayList(delve.graphics.mesh.Mesh) = undefined;
-var entity_meshes: std.ArrayList(delve.graphics.mesh.Mesh) = undefined;
-var cube_mesh: delve.graphics.mesh.Mesh = undefined;
-
-// quake maps load at a different scale and rotation - adjust for that
-var map_transform: math.Mat4 = undefined;
-
-// materials!
-var fallback_quake_material: delve.utils.quakemap.QuakeMaterial = undefined;
-var materials: std.StringHashMap(delve.utils.quakemap.QuakeMaterial) = undefined;
-
-// lights!
-var lights: std.ArrayList(delve.platform.graphics.PointLight) = undefined;
-
-var fog: delve.platform.graphics.MaterialFogParams = .{};
-var lighting: delve.platform.graphics.MaterialLightParams = .{};
-
-// shader setup
-const lit_shader = delve.shaders.default_basic_lighting;
-const basic_lighting_fs_uniforms: []const delve.platform.graphics.MaterialUniformDefaults = &[_]delve.platform.graphics.MaterialUniformDefaults{ .CAMERA_POSITION, .COLOR_OVERRIDE, .ALPHA_CUTOFF, .AMBIENT_LIGHT, .DIRECTIONAL_LIGHT, .POINT_LIGHTS_16, .FOG_DATA };
 
 pub fn main() !void {
     const example = delve.modules.Module{
@@ -104,9 +73,6 @@ pub fn on_tick(delta: f32) void {
     if (delve.platform.input.isKeyJustPressed(.ESCAPE))
         delve.platform.app.exit();
 
-    // update our game time
-    time += delta;
-
     game_instance.tick(delta);
 }
 
@@ -114,21 +80,6 @@ pub fn on_draw() void {
     render_instance.update(&game_instance);
     render_instance.draw(&game_instance);
 }
-
-// Returns the player start position from the map
-// pub fn getPlayerStartPosition(map: *delve.utils.quakemap.QuakeMap) math.Vec3 {
-//     for (map.entities.items) |entity| {
-//         if (std.mem.eql(u8, entity.classname, "info_player_start")) {
-//             const offset = entity.getVec3Property("origin") catch {
-//                 delve.debug.log("Could not read player start offset property!", .{});
-//                 break;
-//             };
-//             return offset;
-//         }
-//     }
-//
-//     return math.Vec3.new(0, 0, 0);
-// }
 
 pub fn cvar_toggleNoclip() void {
     if (game_instance.player.state.move_mode != .NOCLIP) {
