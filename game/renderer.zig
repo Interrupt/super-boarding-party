a4c1604aeee04bfc20641a9308f2ab2160c2391a
Interrupt <cuddigan@gmail.com>
2024-10-12 21:27:47 -0700

    Gizmo is a unit size now

--- game/renderer.zig
+++ game/renderer.zig
@@ -162,10 +162,13 @@ pub const RenderInstance = struct {
     }
 
     pub fn drawDebugTranslateGizmo(self: *RenderInstance, pos: math.Vec3, size: math.Vec3, dir: math.Vec3) void {
-        self.drawDebugCube(pos, math.Vec3.x_axis, math.Vec3.new(2, 0.1, 0.1).mul(size), dir, delve.colors.green);
-        self.drawDebugCube(pos, math.Vec3.y_axis, math.Vec3.new(0.1, 2, 0.1).mul(size), dir, delve.colors.red);
-        self.drawDebugCube(pos, math.Vec3.z_axis, math.Vec3.new(0.1, 0.1, 2).mul(size), dir, delve.colors.blue);
-        self.drawDebugCube(pos, math.Vec3.zero, math.Vec3.new(0.15, 0.15, 0.15).mul(size), dir, delve.colors.white);
+        const thickness: f32 = 0.075;
+        const node_size: f32 = 0.125;
+
+        self.drawDebugCube(pos, math.Vec3.x_axis.scale(0.5).mul(size), math.Vec3.new(1, thickness, thickness).mul(size), dir, delve.colors.green);
+        self.drawDebugCube(pos, math.Vec3.y_axis.scale(0.5).mul(size), math.Vec3.new(thickness, 1, thickness).mul(size), dir, delve.colors.red);
+        self.drawDebugCube(pos, math.Vec3.z_axis.scale(0.5).mul(size), math.Vec3.new(thickness, thickness, 1).mul(size), dir, delve.colors.blue);
+        self.drawDebugCube(pos, math.Vec3.zero, math.Vec3.new(node_size, node_size, node_size).mul(size), dir, delve.colors.white);
     }
 };
 
