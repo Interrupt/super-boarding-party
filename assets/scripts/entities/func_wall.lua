74d0550aa52af7ad8a2546d723e9ddd2a94acd15
Interrupt <cuddigan@gmail.com>
2026-06-01 19:03:39 -0700

    Fixing func_detail walls causing collision

--- assets/scripts/entities/func_wall.lua
+++ assets/scripts/entities/func_wall.lua
@@ -20,7 +20,7 @@ function pkg.MapSpawn(entity, quake_map, quake_entity_idx)
 
 	-- Default entity setup
 	local new_entity = NewEntity(entity, quake_map)
-	local collides_entities = entity.classname ~= "func_illusionary" or entity.classname ~= "func_detail"
+	local collides_entities = entity.classname ~= "func_illusionary" and entity.classname ~= "func_detail"
 
 	-- Make the quake solid
 	local solid = QuakeSolidsComponent.default()
